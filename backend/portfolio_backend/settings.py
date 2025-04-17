249ab5fe1ee032b04cd9592cd9f391d7388a41cf
Muhammad Haroon Hafeez <153822818+Haroon122@users.noreply.github.com>
2025-04-17 04:08:40 -0700

    Update settings.py

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-+1v%jw7&#0fsed2qk=ueph!7^9lo-k-*go06q9^934qd-l@8x=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.148.25.10', 'localhost']
+ALLOWED_HOSTS = [ALLOWED_HOSTS = ['myportfolio.pythonanywhere.com']
+]
 
 
 # Application definition
