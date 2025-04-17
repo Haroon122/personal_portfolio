f90e96b4a3ce8bee9b966e8172e9855dc2db53ea
Muhammad Haroon Hafeez <153822818+Haroon122@users.noreply.github.com>
2025-04-17 05:26:43 -0700

    Update wsgi.py

--- backend/portfolio_backend/wsgi.py
+++ backend/portfolio_backend/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for portfolio_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the sys.path
+project_home = '/home/haroon122/personal_portfolio/backend'  # Change 'yourusername' to your actual username
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
+# Activate your virtual environment
+activate_this = '/home/haroon122/personal_portfolio/env/bin/activate_this.py'  # Update with your virtualenv path
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings')
 
+# Import Django's WSGI application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
